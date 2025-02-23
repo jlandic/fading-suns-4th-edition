fe820750b78bd739e7724cf2516e466759fce72f
Jonathan Landicheff <jlandicheff@kilig.io>
2025-02-23 21:45:29 +0100

    Fix SimpleItemWithTypeSheet inheritance

--- module/sheets/item/simple-item-with-type.mjs
+++ module/sheets/item/simple-item-with-type.mjs
@@ -1,13 +1,7 @@
 import { config } from "../../data/item/_module.mjs";
-import ItemSheetFS4 from "../item-sheet.mjs";
+import SimpleItemSheetFS4 from "./simple-item-sheet.mjs";
 
-export default class SimpleItemWithTypeSheetFS4 extends ItemSheetFS4 {
-  static get defaultOptions() {
-    return foundry.utils.mergeObject(super.defaultOptions, {
-      height: 400,
-    });
-  }
-
+export default class SimpleItemWithTypeSheetFS4 extends SimpleItemSheetFS4 {
   get template() {
     return `systems/fs4/templates/item/simple-item-with-type.hbs`;
   }
