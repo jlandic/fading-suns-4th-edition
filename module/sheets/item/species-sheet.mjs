f5df8943f178722e33b6553d0716557b31a20d45
Jonathan Landicheff <jlandicheff@kilig.io>
2025-09-27 17:13:24 +0200

    fix: species sheets using deleted partials

--- module/sheets/item/species-sheet.mjs
+++ module/sheets/item/species-sheet.mjs
@@ -1,15 +1,11 @@
 import ItemSheetFS4 from "./item-sheet.mjs";
 
 export default class SpeciesSheetFS4 extends ItemSheetFS4 {
-  async getData(options) {
-    const context = await super.getData(options);
-    const item = context.item;
+  async _prepareContext(options) {
+    const context = await super._prepareContext(options);
 
     foundry.utils.mergeObject(context, {
-      description: await TextEditor.enrichHTML(item.system.description, {
-        async: true,
-      }),
-      speed: item.system.speed.join("/"),
+      speed: this.item.system.speed.join("/"),
     });
 
     return context;
